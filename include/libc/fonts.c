7a69f5b6799eb00b353c10a7f42fc3c45864c7dc
feverdreme <jeffreyhuang08@gmail.com>
2022-07-03 23:34:56 -0400

    Commened out deprecated functions

--- include/libc/fonts.c
+++ include/libc/fonts.c
@@ -1,9 +1,9 @@
 #include <libc/fonts.h>
 
-void register_char(char c, const fontchar *fc, struct font* f) {
-    f->data[(int)c - 0x21] = fc;
-}
+// void register_char(char c, const fontchar *fc, struct font* f) {
+//     f->data[(int)c - 0x21] = fc;
+// }
 
-// Interfacing with strings
-const fontchar* char_to_fontchar(char c, struct font *fnt);
-void translate_string(char *sbuf, fontchar **outbuf, struct font* fnt);
+// // Interfacing with strings
+// const fontchar* char_to_fontchar(char c, struct font *fnt);
+// void translate_string(char *sbuf, fontchar **outbuf, struct font* fnt);
