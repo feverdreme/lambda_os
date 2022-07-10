dc68781078993e47b337bb3ae0f66fd6e133271e
feverdreme <jeffreyhuang08@gmail.com>
2022-07-10 16:04:22 -0400

    asm files on fonts supported (must do this for cb)

--- extra/mk/libraries.mk
+++ extra/mk/libraries.mk
@@ -1,13 +1,22 @@
 LIBC_SRC := $(wildcard include/libc/*.c)
 LIBC_O := $(patsubst %.c,%.o,$(LIBC_SRC))
+FONTS_SRC := $(wildcard include/fonts/*.asm)
+FONTS_O := $(patsubst %.asm,%.o,$(FONTS_SRC))
 
-.PHONY: libraries libc
+.PHONY: libraries libc fonts
 
 ############################################################################
 # LIBC BUILD
-libraries: libc
+libraries: libc fonts
 
 libc: $(LIBC_O)
 	$(LD) -relocatable $(LIBC_O) -o bin/objects/libc.o
+
+fonts: $(FONTS_O)
+	$(LD) -relocatable $(FONTS_O) -o bin/objects/fonts.o
+
+# TODO: Explicit asm rules to be defined with wildcard
+$(FONTS_O):
+	$(ASM) -f elf $(patsubst %.o,%.asm,$@) -o $@
 
 ############################################################################
