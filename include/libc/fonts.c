9b31f0cbce08e5af476014cee51cc9b19089279a
feverdreme <jeffreyhuang08@gmail.com>
2022-08-02 00:32:40 -0400

    type stuff

--- include/libc/fonts.c
+++ include/libc/fonts.c
@@ -1,13 +1,12 @@
 #include <libc/fonts.h>
 
-fontchar* ctofc(char c, struct font *fnt) {
-    if (0 <= c) return fnt->data[(int)c];
-    else return 0;
+fontchar ctofc(char c, struct font *fnt) {
+    return fnt->data[c];
 }
 
 void translate_string(char *sbuf, fontchar **outbuf, size_t outbuf_len, struct font* fnt) {
     // is it an antipattern to use outbuf as the incremetned pointer?
-    struct fontchar** out_i = outbuf;
+    fontchar** out_i = outbuf;
 
     for (char *s_i = sbuf; *s_i != '\0'; s_i++) {
         if (out_i - outbuf + 1 > outbuf_len) break;
