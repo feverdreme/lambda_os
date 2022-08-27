8f1d743780ab3ad4e0864a0a16f62966c21a434c
feverdreme <jeffreyhuang08@gmail.com>
2022-08-27 17:59:19 -0400

    typing changes

--- kernel/afterboot/idt.c
+++ kernel/afterboot/idt.c
@@ -1,23 +1,23 @@
 #include "idt.h"
 
-void idt_set_gate(uint8_t index, uint32_t offset, int ss_rpl : 2, int ss_ti : 1, int ss_index : 13, enum idt_gate_type gate_type, int dpl : 2, int present : 1) {
+void idt_set_gate(uint8_t index, uint32_t offset, struct segment_selector seg, idt_gate_type_t gate_type, uint8_t dpl, uint8_t present) {
     struct idt_entry *gate = &(idt[index]);
 
     gate->offset_low = offset & 0xFFFF;
     gate->offset_high = offset >> 16;
-    gate->seg_selector = seg_selector;
     gate->reserved = 0;
 
     gate->type_attributes.gate_type = gate_type;
     gate->type_attributes.dpl = dpl;
     gate->type_attributes.present = present;
     gate->type_attributes.reserved = 0;
+
+    gate->seg_selector = seg;
 }
 
 void idt_init() {
     // set the gates
 
     // prepare for lidt
-
-
+    idt_load();
 }
--- kernel/afterboot/idt.h
+++ kernel/afterboot/idt.h
@@ -2,14 +2,29 @@
 #define IDT_H
 
 #include <stdbool.h>
+#include <stdint.h>
+
+extern struct gdt_entry gdt[3];
 
 // interrupt gate descriptor type
-enum igt_gate_type {
+enum idt_gate_type {
     task = 0x5,
     int_16 = 0x6,
     trap_16 = 0x7,
     int_32 = 0xe,
     trap_32 = 0xf
+};
+typedef enum idt_gate_type idt_gate_type_t;
+
+struct segment_selector {
+    int rpl : 2;
+    int ti : 1;
+    int index : 13;
+} __attribute__((packed));
+
+enum seg_selectors {
+    GDT_CS_0 = (uint16_t)(&gdt[1] << 3 + 0b100);
+    GDT_DS_0 = (uint16_t)(&gdt[2] << 3 + 0b100);
 };
 
 struct idt_type_attributes {
@@ -17,12 +32,6 @@ struct idt_type_attributes {
     int reserved : 1;
     int dpl : 2; // cpu priveledge
     int present : 1; // MUST BE 1 TO BE ACTIVE
-} __attribute__((packed));
-
-struct segment_selector {
-    int rpl : 2;
-    int ti : 1;
-    int index : 13;
 } __attribute__((packed));
 
 struct idt_entry {
@@ -44,7 +53,7 @@ struct idt_ptr idtp;
 // lidt instruction
 extern void idt_load();
 
-void idt_set_gate(uint8_t index, uint32_t offset, struct segment_selector seg_selector, enum idt_gate_type gate_type, uint8_t dpl, bool present);
+void idt_set_gate(uint8_t index, uint32_t offset, struct segment_selector seg, idt_gate_type_t gate_type, uint8_t dpl, uint8_t present);
 
 void idt_init();
 
