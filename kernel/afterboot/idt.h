f720ca3922278926d8571d6040f21110370144c4
feverdreme <jeffreyhuang08@gmail.com>
2022-10-24 01:07:02 -0400

    gdt and idt source only on 1 file

--- kernel/afterboot/idt.h
+++ kernel/afterboot/idt.h
@@ -46,8 +46,26 @@ struct idt_ptr idtp;
 // lidt instruction
 extern void idt_load();
 
-void idt_set_gate(uint8_t index, uint32_t offset, struct segment_selector seg, idt_gate_type_t gate_type, uint8_t dpl, uint8_t present);
+void idt_set_gate(uint8_t index, uint32_t offset, struct segment_selector seg, idt_gate_type_t gate_type, uint8_t dpl, uint8_t present) {
+    struct idt_entry *gate = &(idt[index]);
 
-void idt_init();
+    gate->offset_low = offset & 0xFFFF;
+    gate->offset_high = offset >> 16;
+    gate->reserved = 0;
+
+    gate->type_attributes.gate_type = gate_type;
+    gate->type_attributes.dpl = dpl;
+    gate->type_attributes.present = present;
+    gate->type_attributes.reserved = 0;
+
+    gate->seg_selector = seg;
+}
+
+void idt_init() {
+    // set the gates
+
+    // prepare for lidt
+    idt_load();
+}
 
 #endif
