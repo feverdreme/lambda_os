728fd113b1c1238f606b187e00875acd73187546
feverdreme <jeffreyhuang08@gmail.com>
2022-12-02 02:35:00 -0500

    new memory manager

--- kernel/memory/kmalloc.c
+++ kernel/memory/kmalloc.c
@@ -1,69 +1,109 @@
+#include <libc/math.h>
 #include "kmalloc.h"
 
-const int BSS_BEGIN = 0x3E00; // arbitrary
+int memused = 0;
+int memleft = BSS_SIZE;
+int next_mem_id = 0; // make this a function that can retrieve. we can store all used memids, using this memory model to create a vecotr, then scan for lowest number
 
-const uint32_t MAXMEM = 0x10000;
-
-uint32_t memused = 0;
-uint32_t memleft = MAXMEM;
-mblockptr_t MEMROOT; 
-mblockptr_t last_block;
+MAT_entry_t MAT[MAT_SIZE];
 
 void init_mem_model() {
-    // dirty memory setup at the address
-    MEMROOT = (struct memblock*)(BSS_BEGIN);
-    MEMROOT->addr_loc = BSS_BEGIN;
-    MEMROOT->chunk_size = 1;
-    MEMROOT->prev = MEMROOT;
-    MEMROOT->next = MEMROOT; // if it terminates then it points to itself
-
-    last_block = MEMROOT;
-}
-
-mblockptr_t kmalloc(uint32_t req_size) {
-    if (req_size > memleft) return -1;
-
-    // create a new block
-
-    // set the pointer to a new block
-    uint32_t newaddr = last_block->addr_loc + sizeof(struct memblock) + last_block->chunk_size + 1;
-    last_block->next = (struct memblock*)newaddr;
-
-    // set the next block's prev to be the current block
-    last_block->next->prev = last_block;
-
-    // finally update the last_block pointer
-    last_block = last_block->next;
-
-    // update the values
-    last_block->addr_loc = newaddr;
-    last_block->chunk_size = req_size;
-    last_block->next = last_block;
-
-    // update global memory
-    memused += last_block - last_block->prev;
-    memleft -= last_block - last_block->prev;
-
-    return last_block;
-}
-
-void kfree(struct memblock* block) {
-    // preserve the prev and last blocks
-    struct memblock* prevblock = block->prev;
-    struct memblock* nextblock = block->next;
-    
-    // case for if this is the last block
-    if (nextblock == block) {
-        prevblock->next = prevblock;
-
-        memused -= block - prevblock;
-        memleft += block - prevblock;
-    } else {
-        // case for when it's a middle block
-        prevblock->next = nextblock;
-        nextblock->prev = prevblock;
-
-        memused -= sizeof(struct memblock) + block->chunk_size;
-        memleft += sizeof(struct memblock) + block->chunk_size;
+    for (int i=0; i<MAT_SIZE; i++) {
+        MAT[i].memid = FREE_BLOCK;
+        MAT[i].next_free_blocks = 0;
+        MAT[i].prev_free_blocks = 0;
     }
+
+    MAT[0].next_free_blocks = MAT_SIZE;
+    MAT[MAT_SIZE-1].prev_free_blocks = MAT_SIZE;
+}
+
+void* kmalloc(int req_size) {
+    int needed_blocks = ceildiv(req_size, MBLOCK_SIZE);
+
+    // find the next available free block that satisfies the req_size    
+    int starting_index = -1;
+
+    for (int i=0; i<MAT_SIZE; i++) {
+        if (MAT[i].memid == FREE_BLOCK) continue;
+        if (MAT[i].next_free_blocks >= needed_blocks) {
+            starting_index = i;
+            break;
+        }
+
+        // make this a circular buffer
+        if (i == MAT_SIZE - 1) starting_index = 0;
+    }
+
+    // convert the index to a pointer
+    void* ret_ptr = (void*)(BSS_BEGIN + starting_index * MBLOCK_SIZE);
+
+    // mark all the blocks as used now
+    for (int offset=0; offset<needed_blocks; i++) {
+        MAT[starting_index + offset].memid = next_mem_id;
+    }
+
+    // mark the free blocks
+    mark_contiguous_free_blocks(starting_index - 1);
+    mark_contiguous_free_blocks(starting_index + needed_blocks + 1);
+
+    // return the memory ptr
+    return ret_ptr;
+}
+
+void kfree(void* ptr) {
+    MAT_entry_t* original_entry = calculate_entry_mapping(ptr);
+
+    // floodfill the memid
+    int memid = original_entry->memid;
+
+    MAT_entry_t* left_ptr = original_entry;
+    MAT_entry_t* right_ptr = original_entry + 1;
+
+    while(left_ptr->memid != memid) {
+        left_ptr->memid = FREE_BLOCK;
+        left_ptr--;
+    }
+
+    while(right_ptr->memid != memid) {
+        right_ptr->memid = FREE_BLOCK;
+        right_ptr++;
+    }
+
+    // update the free blocks
+    mark_contiguous_free_blocks((original_entry - MAT) / sizeof(MAT_entry_t));
+}
+
+void mark_contiguous_free_blocks(int free_block_index) {
+    // edge cases
+    if (free_block_index < 0) return;
+    if (free_block_index >= MAT_SIZE) return;
+    if (MAT[free_block_index].memid != FREE_BLOCK) return;
+
+    // scan everything
+    int begin_ptr = free_block_index;
+    int end_ptr = free_block_index;
+
+    while (MAT[end_ptr].memid == FREE_BLOCK && end_ptr < MAT_SIZE) {
+        MAT[end_ptr].next_free_blocks = 0;
+        MAT[end_ptr].prev_free_blocks = 0;
+        end_ptr++;
+    }
+    if (MAT[end_ptr].memid != FREE_BLOCK) end_ptr--;
+
+    while (MAT[begin_ptr].memid == FREE_BLOCK && begin_ptr >= 0) {
+        MAT[begin_ptr].next_free_blocks = 0;
+        MAT[begin_ptr].prev_free_blocks = 0;
+        begin_ptr--;
+    }
+    if (MAT[begin_ptr].memid != FREE_BLOCK) begin_ptr++;
+
+    // mark the corresponding indices
+    int free_block_size = end_ptr - begin_ptr + 1;
+    MAT[begin_ptr].next_free_blocks = free_block_size;
+    MAT[end_ptr].prev_free_blocks = free_block_size;
+}
+
+MAT_entry_t* calculate_entry_mapping(void* ptr) {
+    return (MAT_entry_t*)(MAT + (ptr - BSS_BEGIN) / MBLOCK_SIZE);
 }
