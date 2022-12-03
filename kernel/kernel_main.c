c55a9c5123fcfa1ede0c994f09d7dad3528ce174
feverdreme <jeffreyhuang08@gmail.com>
2022-12-03 01:30:27 -0500

    more experimentation

--- kernel/kernel_main.c
+++ kernel/kernel_main.c
@@ -1,16 +1,17 @@
-#include <libc/fonts.h>
-#include <libc/string.h>
-#include <libc/typedef.h>
+#include <libc/libc.h>
 
-#include "memory/kmalloc.h"
-#include "graphics/putpixel.h"
-#include "graphics/puts.h"
-#include "arch/gdt/gdt.h"
-#include "arch/idt/idt.h"
+#include <memory/kmalloc.h>
+#include <graphics/putpixel.h>
+#include <graphics/puts.h>
+#include <arch/gdt/gdt.h>
+#include <arch/idt/idt.h>
+#include <arch/debug/kpanic.h>
 
 #include <wmgr/window.h>
 
 #include <stdbool.h>
+
+// TODO: make libc and fonts shared static libraries to be linked last
 
 void main() {
     gdt_install();
@@ -18,19 +19,34 @@ void main() {
     init_spleen_font();
     init_mem_model();
 
-    struct memblock* mb = kmalloc(41);
-    *(int*)(mb->data) = 0x0000000F;
+    void* ptr = kmalloc(0x100);
+    printd((int)ptr, &spleen_font);
 
-    char buf[200];
-    itoa(*(int*)(mb->data), buf);
+    println();
 
-    puts(buf, &spleen_font);
-    puts("\n", &spleen_font);
+    for (int i=0; i<18; i++){
+        printd(MAT[i].memid, &spleen_font);
+        printc(' ', &spleen_font);
+    }
 
-    struct memblock* mb2 = kmalloc(40);
-    char buf2[200];
-    itoa(MEMROOT, buf2);
-    puts(buf2, &spleen_font);
+    println();
+
+    char buf[21];
+    itoa(-12345, buf);
+    prints(buf, &spleen_font);
+    // ptr = kmalloc(64);
+    // *(int*)ptr = ceildiv(64, 16);
+    // char buf[20];
+    // itoa(*(int*)ptr, buf);
+
+    // puts("hi", 39, 0, &spleen_font);
+    // prints(buf, &spleen_font);
+    // printc('\n', &spleen_font);
+    // printd(53535, &spleen_font);
+    // printc('\n', &spleen_font);
+    // prints("\nHi2", &spleen_font);
+
+    // prints(buf, &spleen_font);
 
     // puts("Guess who has all the fonts correct... this guy!\nIt even supports newline\nAnd numbers... 123456789\nAnd symbols... -[@$(*%^&(#:>:}>", &spleen_font);
 
