f37314b0368ae2f938bcb92bba27caf3dfb9847e
feverdreme <jeffreyhuang08@gmail.com>
2022-12-03 01:30:10 -0500

    kpanic function

---
+++ kernel/arch/debug/kpanic.c
@@ -0,0 +1,18 @@
+#include "kpanic.h"
+#include "../../graphics/puts.h"
+#include "../../graphics/putpixel.h"
+
+
+void kpanic(const char* msg){
+    // clear first line
+    putrect(0, 0, 320, 8, LIGHTGRAY);
+
+    cursor.x = 0;
+    cursor.y = 0;
+    cursor.kerning = 1;
+
+    prints("KERNEL PANIC: ", &spleen_font);
+    prints(msg, &spleen_font);
+
+    asm("hlt");
+}
