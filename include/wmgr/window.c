409edd70ca4c2c3b5dd3982c018df28f31187913
feverdreme <jeffreyhuang08@gmail.com>
2022-12-03 21:31:45 -0500

    commented out code that doesn't work yet

--- include/wmgr/window.c
+++ include/wmgr/window.c
@@ -1,28 +1,28 @@
-#include <wmgr/window.h>
+// #include <wmgr/window.h>
 
-#include "../kernel/memory/kmalloc.h"
-#include "../kernel/graphics/putpixel.h"
+// #include "../kernel/memory/kmalloc.h"
+// #include "../kernel/graphics/putpixel.h"
 
-const int MAXWINDOWS = 256;
+// const int MAXWINDOWS = 256;
 
-int num_windows = 0;
-Window_Entry_t* all_windows[MAXWINDOWS];  // maximum windows allowed
+// int num_windows = 0;
+// Window_Entry_t* all_windows[MAXWINDOWS];  // maximum windows allowed
 
-Window_t* create_window(int _posx, int _posy, int _th, char* _bodytext) {
-    Window_t* new_window = (Window_t*)kmalloc(sizeof(Window_t));
+// Window_t* create_window(int _posx, int _posy, int _th, char* _bodytext) {
+//     Window_t* new_window = (Window_t*)kmalloc(sizeof(Window_t));
 
-    all_windows[num_windows] = new_window;
-    num_windows++;
-}
+//     all_windows[num_windows] = new_window;
+//     num_windows++;
+// }
 
-void draw_window(Window_t* w){
-    draw_tilebar(w);
-}
+// void draw_window(Window_t* w){
+//     draw_tilebar(w);
+// }
 
-void draw_tilebar(Window_t* w) {
-    // create background
-    putrect(0, 0, 320, 20, LIGHTGRAY);
+// void draw_tilebar(Window_t* w) {
+//     // create background
+//     putrect(0, 0, 320, 20, LIGHTGRAY);
 
-    // put the title text
+//     // put the title text
     
-}
+// }
--- include/wmgr/window.h
+++ include/wmgr/window.h
@@ -1,20 +1,20 @@
-#ifndef WINDOW_H
-#define WINDOW_H
+// #ifndef WINDOW_H
+// #define WINDOW_H
 
-typedef struct Window {
-    int posx;
-    int posy;
-    int tilebar_height;
-    char* bodytext;
-} Window_t;
+// typedef struct Window {
+//     int posx;
+//     int posy;
+//     int tilebar_height;
+//     char* bodytext;
+// } Window_t;
 
-extern const int MAXWINDOWS;
-extern int num_windows;
-extern Window_Entry_t **all_windows; // list of all window pointers
+// extern const int MAXWINDOWS;
+// extern int num_windows;
+// extern Window_Entry_t **all_windows; // list of all window pointers
 
-Window_t* create_window(int _posx, int _posy, int _th, char* _bodytext);
+// Window_t* create_window(int _posx, int _posy, int _th, char* _bodytext);
 
-void draw_window(Window_t* w);
-void draw_tilebar(Window_t* w);
+// void draw_window(Window_t* w);
+// void draw_tilebar(Window_t* w);
 
-#endif
+// #endif
