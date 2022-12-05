3e9b655f412e65b38100a7c32a2bc2eda6cfacf4
feverdreme <jeffreyhuang08@gmail.com>
2022-12-05 01:15:14 -0500

    removed extraneous code

--- source/kernel/kernel_main.c
+++ source/kernel/kernel_main.c
@@ -15,59 +15,11 @@
 
 // TODO: make libc and fonts shared static libraries to be linked last
 
-void report_memory(int num) {
-    for (int i=0; i<num; i++){
-        printd(MAT[i].memid);
-        printc(' ');
-    }
-}
-
 void main() {
     gdt_install();
     idt_init();
     init_spleen_font();
     init_mem_model();
-
-    void* ptr = kmalloc(32);
-    void* ptr2 = kmalloc(16 * 3);
-    void* ptr3 = kmalloc(16);
-
-    kfree(ptr2);
-
-    report_memory(8);
-
-    // printd(1 + false, &spleen_font);
-
-    // void* ptr = kmalloc(0x100);
-    // printd((int)ptr, &spleen_font);
-
-    // println();
-
-    // for (int i=0; i<18; i++){
-    //     printd(MAT[i].memid, &spleen_font);
-    //     printc(' ', &spleen_font);
-    // }
-
-    // println();
-
-    // char buf[21];
-    // itoa(-12345, buf);
-    // prints(buf, &spleen_font);
-    // ptr = kmalloc(64);
-    // *(int*)ptr = ceildiv(64, 16);
-    // char buf[20];
-    // itoa(*(int*)ptr, buf);
-
-    // puts("hi", 39, 0, &spleen_font);
-    // prints(buf, &spleen_font);
-    // printc('\n', &spleen_font);
-    // printd(53535, &spleen_font);
-    // printc('\n', &spleen_font);
-    // prints("\nHi2", &spleen_font);
-
-    // prints(buf, &spleen_font);
-
-    // puts("Guess who has all the fonts correct... this guy!\nIt even supports newline\nAnd numbers... 123456789\nAnd symbols... -[@$(*%^&(#:>:}>", &spleen_font);
 
     return;
 }
