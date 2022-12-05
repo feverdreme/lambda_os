a8ecad104ba11356ed096768de5a8338b71863f0
feverdreme <jeffreyhuang08@gmail.com>
2022-12-05 02:00:16 -0500

    printf makes no sense

--- source/include/libc/printf.h
+++ source/include/libc/printf.h
@@ -4,11 +4,6 @@
 #include <stdarg.h>
 
 // Supports %d ONLY
-void printf(const char* fmt, ...){
-    va_list valist;
-    va_start(valist, fmt);
-
-    
-}
+void printf(const char* fmt, ...);
 
 #endif
