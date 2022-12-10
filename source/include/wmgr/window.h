ed3cd915eec851a1933c2d4e607e513980916b39
feverdreme <jeffreyhuang08@gmail.com>
2022-12-10 00:23:43 -0500

    windowing is dynamic and semi scalable

--- source/include/wmgr/window.h
+++ source/include/wmgr/window.h
@@ -1,6 +1,8 @@
 #ifndef WINDOW_H
 #define WINDOW_H
 
+#include <graphics/putpixel.h>
+
 /* -------------------------------------------------------------------------- */
 /*                                  CONSTANTS                                 */
 /* -------------------------------------------------------------------------- */
@@ -14,7 +16,14 @@
 typedef struct Window {
     int pos_x;
     int pos_y;
+    int width;
+    int height;
+
+    const char *tilebar_title;
     int tilebar_height;
+
+    enum VGA_COLOR border_color;
+
     const char* bodytext;
 } Window_t;
 
@@ -32,10 +41,11 @@ extern Window_t *all_windows[MAX_WINDOWS]; // list of all window pointers
 // Special init function
 void init_genesis_window();
 
-Window_t* create_window(int _pos_x, int _pos_y, int _th, char* _bodytext);
+Window_t* create_window(int _pos_x, int _pos_y, int _width, int _height, const char *_tilebar_title, int _th, enum VGA_COLOR _border_color, char* _bodytext);
 
 void window_draw_window(Window_t* w);
 void window_draw_tilebar(Window_t* w);
 void window_draw_body(Window_t* w);
+void window_draw_border(Window_t* w);
 
 #endif
