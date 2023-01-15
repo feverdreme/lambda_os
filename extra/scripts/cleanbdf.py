9310eccf16187e6884b99abf740f5438afd9275f
feverdreme <jeffreyhuang08@gmail.com>
2023-01-14 22:55:22 -0500

    new spleen font format

--- extra/scripts/cleanbdf.py
+++ extra/scripts/cleanbdf.py
@@ -57,12 +57,12 @@ __{name}_char_{i}:
     return f"""
 ; {name} font declaration
 ; This standard allows c to extern this and reinterpret this as a c-defined struct
-[bits 32]
+[bits 64]
 
 section .data
 
-global _{name}_font
-_{name}_font:
+global {name}_font
+{name}_font:
 \tdb 5, 8 ; width and height; deprecated actually
 \t{name}_font_data:
 
@@ -74,45 +74,82 @@ def generate_asm2(name: str, extracted_chars: list[Fontchar]):
     db_contents = []
     ptr_contents = []
     MOV_TEMPLATE = f"""\
-\tmov ebx, __{name}_char_%d
-\tmov [{name}_font_data + %d], ebx"""
+\tmov rbx, __{name}_char_%d
+\tmov [{name}_font_data + %d], rbx"""
 
     for ec in extracted_chars:
         db_contents.append(f"__{name}_char_{ec.encoding}:\n\t{'db ' + ', '.join(ec.bitmap)}")
-        ptr_contents.append(MOV_TEMPLATE % (ec.encoding, ec.encoding * 4))
+        ptr_contents.append(MOV_TEMPLATE % (ec.encoding, ec.encoding * 8)) # 8 bytes for 64bit
 
     ptr_contents = '\n'.join(ptr_contents)
     db_contents = '\n'.join(db_contents)
 
-    return f"""
+    return f"""\
 ; {name} font declaration
 ; This standard allows c to extern this and reinterpret this as a c-defined struct
-[bits 32]
+[bits 64]
 section .text
 global init_{name}_font
 init_{name}_font:
-\tmov dword [{name}_font], {base_fc.dim[0]}
-\tmov dword [{name}_font + 4], {base_fc.dim[1]}
+\tmov qword [{name}_font], {base_fc.dim[0]}
+\tmov qword [{name}_font + 8], {base_fc.dim[1]}
 {ptr_contents}
 ret
 section .data
 global {name}_font
 {name}_font:
-\tdd 0, 0 ; width and height
-\t{name}_font_data: times 128 dd 0 ; pointer array (32bit)
+\tdq 0, 0 ; width and height
+\t{name}_font_data: times 128 dq 0 ; pointer array (64bit)
 {db_contents}
 """
-	
+
+def generate_asm3(name: str, extracted_chars: list[Fontchar]):
+    """
+        Font specs
+
+        uint32_t width
+        uint32_t height
+        bool[128] is_printable
+        byte[8][128] data
+    """
+    fc_width, fc_height = extracted_chars[0].dim
+
+    char_bmaps: list[list[str]] = [["0x0"] * 8 for _ in range(128)]
+    is_printable = ["0x0" for _ in range(128)]
+
+    # the characters that are printable
+    for ec in extracted_chars:
+        char_bmaps[ec.encoding] = ec.bitmap
+        is_printable[ec.encoding] = "0x1"
     
+    # create data string
+    db_data = ""
+    for bmap in char_bmaps:
+        db_data += "db " + ", ".join(bmap) + "\n"
+    
+    return f"""
+; {name} font declaration
+; This standard allows c to extern this and reinterpret this as a c-defined struct
+[bits 64]
+section .data
+global {name}_font
+{name}_font:
+
+dw {fc_width}
+dw {fc_height}
+db {", ".join(is_printable)}
+{db_data}
+"""
+
 if __name__ == "__main__":
-    fcontents = get_file_contents("../spleen/spleen-5x8.bdf")
+    fcontents = get_file_contents("../../submodules/spleen/spleen-5x8.bdf")
     extracted_chars = sorted(extract_chars(fcontents), key=Fontchar.get_encoding)
     extracted_chars = list(filter(lambda x: x.encoding <= 127, extracted_chars))
 
-    fname = "../../include/fonts/spleen.asm"
+    fname = "../../source/include/fonts/spleendata.asm"
 
     # generate_hfile("spleen_font", "../../include/fonts/spleen.h", extracted_chars)
-    text = generate_asm2("spleen", extracted_chars)
+    text = generate_asm3("spleen", extracted_chars)
 
     with open(fname, 'w+') as f:
         f.write(text)
