a9d9b32dcb91787b6c125fe232a4ac161dce5206
feverdreme <jeffreyhuang08@gmail.com>
2023-01-19 02:20:01 -0500

    vga colors default

--- source/kernel/graphics/putpixel.h
+++ source/kernel/graphics/putpixel.h
@@ -8,6 +8,8 @@
 typedef unsigned char pixel_color_t;
 typedef volatile unsigned char* vga_ptr;
 
+extern void* fb;
+
 enum VGA_COLOR {
     BLACK = 0,
     BLUE = 1,
@@ -27,16 +29,29 @@ enum VGA_COLOR {
     WHITE = 15
 };
 
-extern void* fb;
-
 #elif defined(ARGB32)
 
 typedef uint32_t pixel_color_t;
 typedef uint32_t* vga_ptr;
 
-extern BOOTBOOT bootboot;               // see bootboot.h
-extern unsigned char environment[4096]; // configuration, UTF-8 text key=value pairs
-extern uint8_t fb;                      // linear framebuffer mapped 
+enum VGA_COLOR {
+    BLACK           = 0x000000,
+    BLUE            = 0x0000FF,
+    GREEN           = 0x00FF00,
+    CYAN            = 0x00FFFF,
+    RED             = 0xFF0000,
+    MAGENTA         = 0xFF00FF,
+    BROWN           = 0x964B00,
+    LIGHTGRAY       = 0xD3D3D3,
+    DARKGRAY        = 0x5A5A5A,
+    LIGHTBLUE       = 0xADD8E6,
+    LIGHTGREEN      = 0x90EE90,
+    LIGHTCYAN       = 0xE0FFFF,
+    LIGHTRED        = 0xFFCCCB,
+    LIGHTMAGENTA    = 0xFF80FF,
+    YELLOW          = 0xFFFF00,
+    WHITE           = 0xFFFFFF
+};
 
 #endif
 
