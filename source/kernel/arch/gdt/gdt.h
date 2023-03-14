5b64d946c056f4c53c3abdb10f296311e3fba1ce
feverdreme <jeffreyhuang08@gmail.com>
2023-03-14 00:39:15 -0400

    gdt documentation

--- source/kernel/arch/gdt/gdt.h
+++ source/kernel/arch/gdt/gdt.h
@@ -5,7 +5,37 @@
 
 // https://wiki.osdev.org/GDT
 
-struct gdt_entry {
+/**
+ * @struct gdt_entry
+ * @brief Defines an entry in the GDT
+ *
+ * @var gdt_entry::limit_low
+ * First 16 bits of the limit (ignored in long mode)
+ * Bits 0-15
+ * 
+ * @var gdt_entry::base_low
+ * First 16 bits of the base (ignored in long mode) 
+ * Bits 0-15
+ * 
+ * @var gdt_entry::base_middle
+ * Next 8 bits of the base (ignored in long mode)
+ * Bits 16-23
+ * 
+ * @var gdt_entry::access
+ * The access byte containing data about this segment
+ * 
+ * @var gdt_entry::limit_high
+ * Next 4 bits of the limit
+ * Bits 16-19
+ * 
+ * @var gdt_entry::flags
+ * 4 bit-flags
+ * 
+ * @var gdt_entry::base_high
+ * Last 8 bits of the base
+ * Bits 24-31
+ */
+struct gdt_entry { * Bits 0-15
     uint16_t limit_low;
     uint16_t base_low;
     uint8_t base_middle;
@@ -25,10 +55,18 @@ struct gdt_ptr gp;
 
 // void set_gdt(uint32_t limit, stuct gdt_entry* base);
 
+/**
+ * @brief Set the gdt object
+ * 
+ */
 void set_gdt();
+
+/**
+ * @brief Reloads the registers and perform a far return to reload CS register
+ * 
+ */
 void gdt_reload();
 
-// Declared because this will only ever be used once
 void gdt_set_gate(int num, uint32_t base, uint32_t limit, uint8_t access, uint8_t flags)
 {
     /* Setup the descriptor base address */
