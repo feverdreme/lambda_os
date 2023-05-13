63e2bedd7205ba6c60195a176539f4cfd9a301e8
feverdreme <jeffreyhuang08@gmail.com>
2023-05-13 17:59:09 -0400

    deleted bootboot config files

--- source/build.sh
+++
@@ -1,8 +0,0 @@
-export PATH="/home/jeffrey/opt/cross64/bin:/home/jeffrey/opt/cross/bin:$PATH"
-
-make os64 V=1
-# qemu-system-x86_64 -drive format=raw,file="bin/os.bin",index=0,if=floppy, -m 128M # -s -S
-qemu-system-x86_64 -drive format=raw,file="build/lambda_os.iso" -m 4G #-s -S
-
-make clean
-make reset
