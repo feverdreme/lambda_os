b3934f54142d1f8e26a81310c1bce988a6548146
feverdreme <jeffreyhuang08@gmail.com>
2023-05-13 11:37:09 -0400

    extracted code from translate to get_vaddr_indices

--- source/kernel/arch/paging/virtm.h
+++ source/kernel/arch/paging/virtm.h
@@ -1,12 +1,16 @@
 #ifndef VIRTM_H
 #define VIRTM_H
 
-/**
- * @brief Translates a virtual address into the physical one
- * 
- * @param vaddr Virtual address
- * @return void* 
- */
-void *translate_virtaddr(void *vaddr);
+#include <stdint.h>
+
+typedef struct translated_vaddr {
+    uint8_t PML4_index;
+    uint8_t PDPT_index;
+    uint8_t PD_index;
+    uint8_t PT_index;
+    uint16_t PHYS_offset;
+} translated_vaddr_t;
+
+translated_vaddr_t get_vaddr_indices(void *vaddr);
 
 #endif
