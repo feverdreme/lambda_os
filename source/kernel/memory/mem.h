d9a9c81613061bb39e07bdb90ced6752ad8487af
feverdreme <jeffreyhuang08@gmail.com>
2023-05-13 15:36:14 -0400

    necesary for further limine ops

--- source/kernel/memory/mem.h
+++ source/kernel/memory/mem.h
@@ -1,6 +1,9 @@
 #ifndef MEM_H
 #define MEM_H
 
+#include <envvars.h>
+#include <stddef.h>
+
 /**
  * @brief Copies data from dest to src in linear fashion
  * 
@@ -8,7 +11,7 @@
  * @param src Source pointer
  * @param len Number of bytes to copy
  */
-void memcpy(void* dest, void* src, int len);
+void *memcpy(void* dest, const void* src, size_t len);
 
 /**
  * @brief Writes data to destination with a byte
@@ -17,7 +20,7 @@ void memcpy(void* dest, void* src, int len);
  * @param value Value to be written
  * @param len Number of bytes to set
  */
-void memset(void* dest, char value, int len);
+void *memset(void* dest, int value, size_t len);
 
 /**
  * @brief Moves data from dest to src all at once. Differs from memcpy by storing data into intermediate buffer
@@ -26,6 +29,16 @@ void memset(void* dest, char value, int len);
  * @param src Source pointer
  * @param len Number of bytes to move
  */
-void memmove(void* dest, void* src, int len);
+void *memmove(void* dest, const void* src, size_t len);
+
+/**
+ * @brief Compares memory data
+ * 
+ * @param s1 First memory section
+ * @param s2 Second memory section 
+ * @param n Size, in bytes
+ * @return int 0 if the memory sections are the same, if not, the first difference
+ */
+int memcmp(const void *s1, const void *s2, size_t n);
 
 #endif
