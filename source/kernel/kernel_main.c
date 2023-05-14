87e8d6b8c454b68c4574de49c0833cec7426b774
feverdreme <jeffreyhuang08@gmail.com>
2023-05-13 23:56:30 -0400

    all new header files integrated

--- source/kernel/kernel_main.c
+++ source/kernel/kernel_main.c
@@ -1,6 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
-#include <bootboot.h>
+#include <limine.h>
 
 #include <envvars.h>
 
@@ -9,6 +9,9 @@
 #include <arch/paging/paging.h>
 
 #include <memory/kmalloc.h>
+#include <memory/mem.h>
+#include <graphics/fb.h>
+#include <graphics/putpixel.h>
 #include <graphics/puts.h>
 #include <arch/debug/kpanic.h>
 #include <wmgr/window.h>
@@ -17,21 +20,22 @@
 // TODO: make libc and fonts shared static libraries to be linked last
 
 void main() {
-    gdt_install();
-    idt_init();
+    initialize_fb();
+    // gdt_install();
+    // idt_init();
     init_mem_model();
-    // init_genesis_window();
+    init_genesis_window();
     Tile_t *genesis_tile = init_genesis_tile();
-    // putrect(0, 0, INTERNAL_WIDTH, INTERNAL_HEIGHT, WHITE);
+    // // putrect(0, 0, FB_WIDTH, FB_HEIGHT, WHITE);
 
-    Window_t* test_window = create_window(0, 0, INTERNAL_WIDTH / 2, INTERNAL_HEIGHT, "Window 1", 20, BLACK, "Hello! test window!");
-    Window_t* test_window2 = create_window(INTERNAL_WIDTH / 2, 0, INTERNAL_WIDTH / 2, INTERNAL_HEIGHT / 2, "Window 2", 20, BLACK, "Pog Pog");
-    Window_t* test_window3 = create_window(INTERNAL_WIDTH / 2, INTERNAL_HEIGHT / 2, INTERNAL_WIDTH / 2, INTERNAL_HEIGHT / 2, "Patricie Viewer", 20, GREEN, "Ancilla est in foro");
-    Window_t* test_window4 = create_window(INTERNAL_WIDTH / 2, 0, INTERNAL_WIDTH / 2, INTERNAL_HEIGHT / 2, "Window 4", 20, BLACK, "Pog Pog 2");
+    Window_t* test_window = create_window(0, 0, FB_WIDTH / 2, FB_HEIGHT, "Window 1", 20, BLACK, "Hello! test window!");
+    Window_t* test_window2 = create_window(FB_WIDTH / 2, 0, FB_WIDTH / 2, FB_HEIGHT / 2, "Window 2", 20, BLACK, "Pog Pog");
+    Window_t* test_window3 = create_window(FB_WIDTH / 2, FB_HEIGHT / 2, FB_WIDTH / 2, FB_HEIGHT / 2, "Patricie Viewer", 20, GREEN, "Ancilla est in foro");
+    Window_t* test_window4 = create_window(FB_WIDTH / 2, 0, FB_WIDTH / 2, FB_HEIGHT / 2, "Window 4", 20, BLACK, "Pog Pog 2");
     
-    // window_draw_window(test_window);
-    // window_draw_window(test_window2);
-    // window_draw_window(test_window3);
+    // // window_draw_window(test_window);
+    // // window_draw_window(test_window2);
+    // // window_draw_window(test_window3);
 
     Tile_t* t1 = create_tile(WINDOW_CHILDREN);
     Tile_t* t2 = create_tile(WINDOW_CHILDREN);
@@ -52,7 +56,7 @@ void main() {
 
     // if (sizeof(Page_Entry_t) == 8)
     tile_draw(genesis_tile, 0, 0, 640, 400);
-    // printd(get_MAXPHYADDR());
+    printd(get_MAXPHYADDR());
 
     return;
 }
