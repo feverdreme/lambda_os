9531a67bbacb682281015ef881ec51a6ba76ef59
feverdreme <jeffreyhuang08@gmail.com>
2023-05-14 00:52:29 -0400

    expanded define statments

--- source/kernel/arch/paging/page_entry_flags.h
+++ source/kernel/arch/paging/page_entry_flags.h
@@ -1,9 +1,9 @@
 #ifndef PAGE_ENTRY_FLAGS_H
 #define PAGE_ENTRY_FLAGS_H
 
-#define PE_RW	(1<<1)	// read/write
-#define PE_US	(1<<2)	// user/supervisor
-#define PE_PWT	(1<<3)	// page-level write-through
-#define PE_PCD	(1<<4)	// page-level cache disable		
+#define PE_READ_WRITE	        (1<<1)	// read/write
+#define PE_USER_SUPERVISOR	    (1<<2)	// user/supervisor
+#define PE_PLEVEL_WRITE_THROUGH	(1<<3)	// page-level write-through
+#define PE_PLEVEL_CACHE_DISABLE	(1<<4)	// page-level cache disable		
 
 #endif
