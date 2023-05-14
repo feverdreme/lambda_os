309142865e414b47e4ce32b1fdf7a76c73c33801
feverdreme <jeffreyhuang08@gmail.com>
2023-05-14 17:25:05 -0400

    moved flags back to paging.h and added more easy-access structs

--- source/kernel/arch/paging/page_entry_flags.h
+++
@@ -1,9 +0,0 @@
-#ifndef PAGE_ENTRY_FLAGS_H
-#define PAGE_ENTRY_FLAGS_H
-
-#define PE_READ_WRITE	        (1<<1)	// read/write
-#define PE_USER_SUPERVISOR	    (1<<2)	// user/supervisor
-#define PE_PLEVEL_WRITE_THROUGH	(1<<3)	// page-level write-through
-#define PE_PLEVEL_CACHE_DISABLE	(1<<4)	// page-level cache disable		
-
-#endif
--- source/kernel/arch/paging/paging.h
+++ source/kernel/arch/paging/paging.h
@@ -4,6 +4,8 @@
 #include <cpuid.h>
 #include <envvars.h>
 
+// This can only map 4kb pages, other direct mappings are too complex right now
+
 static int get_MAXPHYADDR() {
     int cpuid_code = 0x80000008; // EAX code for CPUID call
 
@@ -17,46 +19,63 @@ static int get_MAXPHYADDR() {
 
 // CR4.PCIDE = 0
 
+#define NUM_PDPT                (512)
+#define NUM_PAGE_DIRECTORIES    (512*512)
+#define NUM_PAGE_TABLES         (512*512*512)
+
+#define PE_READ_WRITE	        (1<<1)	// read/write
+#define PE_USER_SUPERVISOR	    (1<<2)	// user/supervisor
+#define PE_PLEVEL_WRITE_THROUGH	(1<<3)	// page-level write-through
+#define PE_PLEVEL_CACHE_DISABLE	(1<<4)	// page-level cache disable		
+
 typedef struct Page_Entry {
     int present : 1;
     int flags : 4;
     int accessed : 1;
     int ignored : 1;
-    int reserved : 1;
+    int reserved : 1;   // in non PML4, this indicates whether it maps a page or another paging structure
     int avl : 4;        // up to me, ignored by MMU
     uint64_t phys : 40; // includes M-1:12 and reserved bits
     int avl2 : 11;
     int xd : 1;         // execute-disable
-} __attribute__((packed)) Page_Entry_t;
+} __attribute__((packed)) 	Page_Entry_t;
 
-typedef Page_Entry_t Paging_Structure_t[512];
+typedef Page_Entry_t 		Paging_Structure_t[512];
 
-typedef Paging_Structure_t Page_Table_t;
-typedef Paging_Structure_t Page_Directory_t;
-typedef Paging_Structure_t PDPT_t;
-typedef Paging_Structure_t PML4_t;
+typedef Paging_Structure_t 	Page_Table_t;
+typedef Paging_Structure_t 	Page_Directory_t;
+typedef Paging_Structure_t 	PDPT_t;
+typedef Paging_Structure_t 	PML4_t;
 
-extern PML4_t PML4T;
+typedef PDPT_t 				Contiguous_PDPT_t[512];
+typedef Page_Directory_t	Contiguous_PD_t[512][512];
+typedef Page_Table_t		Contiguous_PT_t[512][512][512];
+
+extern PML4_t 				*PML4T;
+extern Contiguous_PDPT_t 	*ALL_PDPT;
+extern Contiguous_PD_t 		*ALL_PD;
+extern Contiguous_PT_t 		*ALL_PT;
+
 extern int MAXPHYADDR;
 
 /**
- * @brief Set a page table entry located at a virtual address, along with necesary heirarchical structures. If it already exists returns a pointer to the existing entry.
+ * @brief Set a 4kb page table entry located at a virtual address, along with necesary heirarchical structures. If it already exists returns a pointer to the existing entry. Does not cascade enable.
  * 
  * @param phys_addr 
  * @param vaddr 
  * @return Page_Entry_t* 
  */
-Page_Entry_t *set_page(void *phys_addr, void *vaddr);
+Page_Entry_t *set_page(void *phys_addr, void *vaddr, uint8_t pe_flags);
 
 /**
- * @brief Sets the present bit on a page entry
+ * @brief Sets the present bit on a page entry, cascading down hierarchical structures.
  * 
  * @param pte Page table entry
  */
-void enable_page_entry(Page_Entry_t *pte);
+Page_Entry_t *enable_page_entry(Page_Entry_t *pte);
 
 /**
- * @brief Disables the present bit on a page entry
+ * @brief Disables the present bit on a page entry, and all necesary
  * 
  * @param pte Page table entry
  */
@@ -69,6 +88,12 @@ void disable_page_entry(Page_Entry_t *pte);
  * @return Page_Entry_t* A pointer to the page entry
  */
 Page_Entry_t *locate_page_entry(void *vaddr);
+
+/**
+ * @brief Sets up all the paging structures
+ * 
+ */
+void setup_all_paging_structures();
 
 /**
  * @brief Initializes all paging for the OS, including remapping the BOOTBOOT paging and modifying cr3.
