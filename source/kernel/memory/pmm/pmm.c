376d296e13ea39650052d4452cd475d45a9713b2
feverdreme <jeffreyhuang08@gmail.com>
2023-05-17 04:12:03 -0400

    pmm implemented

---
+++ source/kernel/memory/pmm/pmm.c
@@ -0,0 +1,62 @@
+#include "pmm.h"
+
+#include <limine_requests.h>
+
+struct limine_memmap_response *memmap_response = memmap_request.response;
+
+uint64_t pmm_page_bitmap_usable[PMM_BITMAP_SIZE];
+uint64_t pmm_page_bitmap_used[PMM_BITMAP_SIZE];
+
+void mark_pmm_bitmap(uint64_t *bitmap, uint64_t addr, bool use){
+    // divide address by 4 KiB
+    addr /= PAGE_SIZE;
+
+    // 64 page-refs in one uint64_t
+    int bitmap_index = addr / 64;
+    int bitmap_bit_index = addr % 64;
+
+    if (usable)
+        bitmap[bitmap_index] |= (1 << bitmap_bit_index);
+    else
+        bitmap[bitmap_index] &= ~(1 << bitmap_bit_index);
+}
+
+bool pmm_addr_access_bit(uint64_t *bitmap, uint64_t addr) {
+    addr /= PAGE_SIZE;
+
+    // 64 page-refs in one uint64_t
+    int bitmap_index = addr / 64;
+    int bitmap_bit_index = addr % 64;
+
+    return bitmap[bitmap_index] & (1 << bitmap_bit_index);
+}
+
+void *pmm_alloc_page() {
+    // look through bitmaps
+    for (uint64_t addr = 0x1000; addr += PAGE_SIZE; addr < MAX_MEMORY_SIZE) {
+        if (!pmm_addr_access_bit(pmm_page_bitmap_usable, addr) 
+            || pmm_addr_access_bit(pmm_page_bitmap_used, addr)) continue;
+
+        // we've found a free page
+        mark_pmm_bitmap(pmm_page_bitmap_used, addr, true);
+        return (void*)addr;
+    }
+}
+
+void pmm_free_page(void *addr) {
+    mark_pmm_bitmap(pmm_page_bitmap_used, (uint64_t)addr, false);
+}
+
+void pmm_init() {
+    uint64_t entry_count = memmap_response->entry_count;
+    struct limine_memmap_entry **entries = memmap_response->entries;
+
+    // iterate for usable entries
+    for (int i=0; i<entry_count; i++) {
+        struct limine_memmap_entry entry = *(entries[i]);
+
+        // iterate through all pages marked
+        for (uint64_t base_offset=0; base_offset < entry.length; base_offset += PAGE_SIZE)
+            mark_pmm_bitmap(pmm_page_bitmap_usable, entry.base + base_offset, entry.type == LIMINE_MEMMAP_USABLE);
+    }
+}
---
+++ source/kernel/memory/pmm/pmm.h
@@ -0,0 +1,49 @@
+#ifndef PMM_H
+#define PMM_H
+
+#include <stdint.h>
+#include <stdbool.h>
+
+#define MAX_MEMORY_SIZE 	(4 << 30)   					// 4 GiB
+#define PAGE_SIZE       	(4 << 10)   					// 4 KiB
+#define NUM_POSSIBLE_PAGES	(MAX_MEMORY_SIZE / PAGE_SIZE)
+#define PMM_BITMAP_SIZE		(NUM_POSSIBLE_PAGES / 64)		// number of bits / 64 bit integers
+
+extern uint64_t pmm_page_bitmap_usable[PMM_BITMAP_SIZE];	// bitmap for usable memory regions
+extern uint64_t pmm_page_bitmap_used[PMM_BITMAP_SIZE];		// bitmap that the pmm uses to keep track of freed and alloced pages
+
+/**
+ * @brief Marks the 64bit address corresponding to the bitmap.
+ * 
+ * @param bitmap The bitmap to mutate
+ * @param addr The 64bit address
+ * @param use The bit to set
+ */
+void mark_pmm_bitmap(uint64_t *bitmap, uint64_t addr, bool use);
+
+/**
+ * @brief Determines the status of an address represented in the bitmap
+ * 
+ * @param bitmap Bitmap to access
+ * @param addr The 64bit address
+ * @return The access bit
+ */
+bool pmm_addr_access_bit(uint64_t *bitmap, uint64_t addr);
+
+/**
+ * @brief Allocates a 4KiB-aligned page
+ * 
+ * @return void* The address
+ */
+void *pmm_alloc_page();
+
+/**
+ * @brief Frees the 4KiB-aligned page at an address
+ * 
+ * @param addr The address.
+ */
+void pmm_free_page(void *addr);
+
+void pmm_init();
+
+#endif
