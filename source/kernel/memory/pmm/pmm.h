eb12016ec7baa053b28f62ccc703c290d60b53de
feverdreme <jeffreyhuang08@gmail.com>
2023-05-17 04:32:48 -0400

    fixed array variable size error

--- source/kernel/memory/pmm/pmm.h
+++ source/kernel/memory/pmm/pmm.h
@@ -9,8 +9,8 @@
 #define NUM_POSSIBLE_PAGES	(MAX_MEMORY_SIZE / PAGE_SIZE)
 #define PMM_BITMAP_SIZE		(NUM_POSSIBLE_PAGES / 64)		// number of bits / 64 bit integers
 
-extern uint64_t pmm_page_bitmap_usable[PMM_BITMAP_SIZE];	// bitmap for usable memory regions
-extern uint64_t pmm_page_bitmap_used[PMM_BITMAP_SIZE];		// bitmap that the pmm uses to keep track of freed and alloced pages
+extern uint64_t *pmm_page_bitmap_usable;	// bitmap for usable memory regions
+extern uint64_t *pmm_page_bitmap_used;		// bitmap that the pmm uses to keep track of freed and alloced pages
 
 /**
  * @brief Marks the 64bit address corresponding to the bitmap.
