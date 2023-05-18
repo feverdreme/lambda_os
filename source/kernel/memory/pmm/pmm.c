e510daf532c526f93899233310a9b9055cdda22b
feverdreme <jeffreyhuang08@gmail.com>
2023-05-18 19:13:11 -0400

    fixed the pmm bugs

--- source/kernel/memory/pmm/pmm.c
+++ source/kernel/memory/pmm/pmm.c
@@ -2,6 +2,7 @@
 
 #include <arch/debug/kpanic.h>
 #include <limine_requests.h>
+#include <memory/mem.h>
 
 struct limine_memmap_response *memmap_response;
 
@@ -9,17 +10,19 @@ uint64_t pmm_page_bitmap_usable[PMM_BITMAP_SIZE];
 uint64_t pmm_page_bitmap_used[PMM_BITMAP_SIZE];
 
 void mark_pmm_bitmap(uint64_t *bitmap, uint64_t addr, bool use){
+    if (addr >= ((uint64_t)4<<30)) return;
+
     // divide address by 4 KiB
     addr /= PAGE_SIZE;
 
     // 64 page-refs in one uint64_t
-    int bitmap_index = addr / 64;
-    int bitmap_bit_index = addr % 64;
+    uint64_t bitmap_index = addr / 64;
+    uint64_t bitmap_bit_index = addr % 64;
 
     if (use)
-        bitmap[bitmap_index] |= (1 << bitmap_bit_index);
+        bitmap[bitmap_index] |= ((uint64_t)1 << bitmap_bit_index);
     else
-        bitmap[bitmap_index] &= ~(1 << bitmap_bit_index);
+        bitmap[bitmap_index] &= ~((uint64_t)1 << bitmap_bit_index);
 }
 
 bool pmm_addr_access_bit(uint64_t *bitmap, uint64_t addr) {
@@ -32,23 +35,30 @@ bool pmm_addr_access_bit(uint64_t *bitmap, uint64_t addr) {
     return bitmap[bitmap_index] & (1 << bitmap_bit_index);
 }
 
+// int c = 0;
+
 void *pmm_alloc_page() {
-    uint64_t addr;
+    uint64_t found_addr = 0;
 
-    for (addr = 0x1000; addr += PAGE_SIZE;
-        addr < MAX_MEMORY_SIZE &&
-        (!pmm_addr_access_bit(pmm_page_bitmap_usable, addr) ||
-         pmm_addr_access_bit(pmm_page_bitmap_used, addr))
-    );
+    for (uint64_t addr = 4096; addr < MAX_MEMORY_SIZE; addr += PAGE_SIZE) {
+        if (pmm_addr_access_bit(pmm_page_bitmap_usable, addr) &&  !pmm_addr_access_bit(pmm_page_bitmap_used, addr)) {
+            found_addr = addr;
+            break;
+        }
+    }
 
-    if (addr == MAX_MEMORY_SIZE) {
+    if (found_addr == 0) {
         kpanic("NO FREE PAGE FOUND: ALL MEMORY USED");
         return NULL;
     }
 
+    // if (c++ < 50) {
+    // printd(found_addr);
+    // println();}
+
     // we've found a free page
-    mark_pmm_bitmap(pmm_page_bitmap_used, addr, true);
-    return (void*)addr;
+    mark_pmm_bitmap(pmm_page_bitmap_used, found_addr, true);
+    return (void*)found_addr;
 }
 
 void pmm_free_page(void *addr) {
@@ -56,6 +66,9 @@ void pmm_free_page(void *addr) {
 }
 
 void pmm_init() {
+    memset(pmm_page_bitmap_used, 0, sizeof(uint64_t) * PMM_BITMAP_SIZE);
+    memset(pmm_page_bitmap_usable, 0, sizeof(uint64_t) * PMM_BITMAP_SIZE);
+
     memmap_response = memmap_request.response;
 
     uint64_t entry_count = memmap_response->entry_count;
