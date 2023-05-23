0314384a91f6e77a17d7412a25375c8ac75b20b1
feverdreme <jeffreyhuang08@gmail.com>
2023-05-23 02:35:25 -0400

    lapic logic setup

--- source/kernel/arch/apic/apic.c
+++ source/kernel/arch/apic/apic.c
@@ -2,10 +2,14 @@
 
 #include <arch/msr.h>
 
+uint64_t APIC_PHYSICAL_ADDRESS;
+
 uint64_t get_apic_base() {
     MSR_Response_t apic_base_msr = cpu_read_msr(IA32_APIC_BASE_MSR);
 
 	uint64_t apic_addr = (apic_base_msr.low & 0xfffff000) | ((uint64_t)apic_base_msr.high << 32);
+
+	APIC_PHYSICAL_ADDRESS = apic_addr;
 
 	return apic_addr;
 }
@@ -14,9 +18,27 @@ void enable_apic_base() {
 	MSR_Response_t apic_base_msr = cpu_read_msr(IA32_APIC_BASE_MSR);
 
 	uint32_t low = apic_base_msr.low & 0xfffff000;
-	uint32_t high = apic_base.high;
+	uint32_t high = apic_base_msr.high;
 
 	low |= IA32_APIC_BASE_MSR_ENABLE;
 
 	cpu_write_msr(IA32_APIC_BASE_MSR, low, high);
+}
+
+uint32_t read_lapic_register(int offset) {
+	return *(uint32_t*)(APIC_PHYSICAL_ADDRESS + offset);
+}
+
+void write_lapic_register(int offset, uint32_t data) {
+	*(uint32_t*)(APIC_PHYSICAL_ADDRESS + offset) = data;
+}
+
+void initialize_lapic() {
+	enable_apic_base();
+
+	// configure spurious interrupts
+	uint32_t lapic_spurious_int = read_lapic_register(APIC_SPURIOUS_INT_REG);
+	lapic_spurious_int |= 0xff;		// sets dummy interrupt vector
+	lapic_spurious_int |= (1<<8);	// sets enable-bit
+	write_lapic_register(APIC_SPURIOUS_INT_REG, lapic_spurious_int);
 }
--- source/kernel/arch/apic/apic.h
+++ source/kernel/arch/apic/apic.h
@@ -7,8 +7,12 @@
 #define IA32_APIC_BASE_MSR_ENABLE   (1<<11)
 #define IA32_APIC_BASE_MSR_BSP      (1<<8)
 
+#define APIC_SPURIOUS_INT_REG       0xF0
+
+extern uint64_t APIC_PHYSICAL_ADDRESS;
+
 /**
- * @brief Get the apic physical address.
+ * @brief Get the apic physical address. Side-effect: sets APIC_PHYSICAL_ADDRESS
  * 
  * @return uint64_t 
  */
@@ -19,5 +23,27 @@ uint64_t get_apic_base();
  * 
  */
 void enable_apic_base();
+
+/**
+ * @brief Reads a LAPIC register at resignated offset.
+ * 
+ * @param offset 
+ * @return uint32_t 
+ */
+uint32_t read_lapic_register(int offset);
+
+/**
+ * @brief Writes to a LAPIC register.
+ * 
+ * @param offset 
+ * @param data 
+ */
+void write_lapic_register(int offset, uint32_t data);
+
+/**
+ * @brief Initializes the Local APIC.
+ * 
+ */
+void initialize_lapic();
 
 #endif
