4c16ef8130dbbe484585d24c3b13931fb6374989
feverdreme <jeffreyhuang08@gmail.com>
2023-05-23 03:02:34 -0400

    added ACPI_SDT type definitions

--- source/kernel/arch/acpi/acpi.h
+++ source/kernel/arch/acpi/acpi.h
@@ -10,6 +10,38 @@ typedef struct RSDP_Descriptor {
     uint8_t revision;
     uint32_t rsdt_address;
 } __attribute__ ((packed)) RSDP_Descriptor_t;
+
+typedef struct Extended_RSDP_Descriptor {
+    char signature[8];
+    uint8_t checksum;
+    char OEMID[6];
+    uint8_t revision;
+    uint32_t rsdt_address;
+    
+    // New in revision 2
+    uint32_t length;
+    uint64_t xsdt_address;
+    uint8_t extended_checksum;
+    uint8_t reserved[3];
+} __attribute__ ((packed)) Extended_RSDP_Descriptor_t;
+
+typedef struct ACPI_SDT_Header {
+    char signature[4];
+    uint32_t length;
+    uint8_t revision;
+    uint8_t checksum;
+    char OEMID[6];
+    char OEMTableID[8];
+    uint32_t OEM_revision;
+    uint32_t creator_id;
+    uint32_t creator_revision;
+} ACPI_SDT_Header_t;
+
+typedef struct RSDT {
+    struct ACPISDTHeader header;
+    uint32_t SDT_pointers[(header.length - sizeof(header)) / 4];
+} RSDT_t;
+
 /**
  * @brief Discovers what type of RSDP we're using.
  * 
