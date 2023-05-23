7ff7e8792cc85fbcd046d32b2077cf9b989c2983
feverdreme <jeffreyhuang08@gmail.com>
2023-05-23 01:50:57 -0400

    added enable_apic_base

--- source/kernel/arch/apic/apic.c
+++ source/kernel/arch/apic/apic.c
@@ -2,12 +2,21 @@
 
 #include <arch/msr.h>
 
-#define IA32_APIC_BASE_MSR	0x1B
-
 uint64_t get_apic_base() {
     MSR_Response_t apic_base_msr = cpu_read_msr(IA32_APIC_BASE_MSR);
 
 	uint64_t apic_addr = (apic_base_msr.low & 0xfffff000) | ((uint64_t)apic_base_msr.high << 32);
 
 	return apic_addr;
+}
+
+void enable_apic_base() {
+	MSR_Response_t apic_base_msr = cpu_read_msr(IA32_APIC_BASE_MSR);
+
+	uint32_t low = apic_base_msr.low & 0xfffff000;
+	uint32_t high = apic_base.high;
+
+	low |= IA32_APIC_BASE_MSR_ENABLE;
+
+	cpu_write_msr(IA32_APIC_BASE_MSR, low, high);
 }
--- source/kernel/arch/apic/apic.h
+++ source/kernel/arch/apic/apic.h
@@ -3,11 +3,21 @@
 
 #include <stdint.h>
 
+#define IA32_APIC_BASE_MSR	        0x1B
+#define IA32_APIC_BASE_MSR_ENABLE   (1<<11)
+#define IA32_APIC_BASE_MSR_BSP      (1<<8)
+
 /**
  * @brief Get the apic physical address.
  * 
  * @return uint64_t 
  */
 uint64_t get_apic_base();
+
+/**
+ * @brief Sets the enable flag on the APIC MSR.
+ * 
+ */
+void enable_apic_base();
 
 #endif
