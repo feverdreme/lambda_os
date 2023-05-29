154b224e0b9a7d66ea9a43d8a4a71c4b29b3db79
feverdreme <jeffreyhuang08@gmail.com>
2023-05-29 15:53:18 -0400

    fixed keyboard inb and removed debug functions

--- source/kernel/dev/keyboard.int.c
+++ source/kernel/dev/keyboard.int.c
@@ -4,17 +4,19 @@
 #include <arch/apic/apic.h>
 #include <arch/apic/ioapic.h>
 #include <graphics/puts.h>
+#include <memory/mem.h>
 
 __attribute__((interrupt))
 void keyboard_handler(interrupt_frame_t* frame) {
     // __asm__ volatile ("movl $0xdeadbeef, %ebx");
-    prints("KEYBOARD\n");
+    unsigned char scan_code = inb(0x60);
+    printd(scan_code);
+    println();
+
+    send_lapic_eoi();
 }
 
 void isr_keyboard_register() {
     idt_register_isr(33, &keyboard_handler);
-
     ioapic_write_entry(1, 33, IOREDTBL_FIXED, read_lapic_register(0x20));
-    printh(ioapic_read_entry(1));
-    println();
 }
